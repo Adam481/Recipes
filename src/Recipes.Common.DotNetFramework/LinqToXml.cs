5c64bc1e3cd6bca83dd421ff66c6f6daad36ba97
Adam Maliszewski <Adam481@autograf.pl>
2018-02-08 22:35:27 +0100

    Added old notes: C# fundamentals + linq + regex

---
+++ src/Recipes.Common.DotNetFramework/LinqToXml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Recipes.Common.DotNetFramework
+{
+    internal class LinqToXml
+    {
+        //private static void QuerySqlServer()
+        //{
+        //    // MovieReviewsDataContext dc = new MovieReviewsDataContext();
+        //    //IEnumerable<Movie> topMovies =
+        //    //    from m in dc.Movies
+        //    //    where m.ReleaseData.Year > 2006
+        //    //    orderby m.Reviews.Average(r => r.Rating) descending
+        //    //    select m;
+
+        //    //foreach (int id in topMovies)
+        //    //{
+        //    //    Console.WriteLine(movie.Reviews);
+        //    //}
+        //}
+
+        private static void QueryXml()
+        {
+            XDocument doc = new XDocument(
+                new XElement("Processes",
+                    from p in Process.GetProcesses()
+                    orderby p.ProcessName ascending
+                    select new XElement("Process",
+                        new XAttribute("Name", p.ProcessName),
+                        new XAttribute("PID", p.Id))));
+
+            IEnumerable<int> pids = //from e in doc.Element("Processes").Elements("Process")
+                from e in doc.Descendants("Process")
+                where e.Attribute("Name").Value == "devenv"
+                orderby (int)e.Attribute("PID") ascending
+                select (int)e.Attribute("PID");
+
+            foreach (int id in pids)
+            {
+                Console.WriteLine(id);
+            }
+        }
+
+
+        private static void QueryEmployeesList()
+        {
+            Console.WriteLine("\nQuery employees method");
+
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee { ID=1, Name="Scott", HireDate=new DateTime(2002, 3, 5)},
+                new Employee { ID=2, Name="Poonam", HireDate=new DateTime(2002, 10, 15)},
+                new Employee { ID=3, Name="Paul", HireDate=new DateTime(2007, 10, 11)}
+            };
+
+            IEnumerable<Employee> query =       // query will be executed in foreach loop
+                from e in employees
+                where e.HireDate.Year < 2005
+                orderby e.Name
+                select e;
+
+            employees.Add(new Employee() { Name = "Linda", ID = 4, HireDate = new DateTime(2003, 6, 11) });
+
+            foreach (Employee e in query)
+            {
+                Console.WriteLine(e.Name);
+            }
+
+        }
+
+
+        private static void QueryEmployees()
+        {
+            Console.WriteLine("\nQuery employees method");
+
+            IEnumerable<Employee> employees = new List<Employee>()
+            {
+                new Employee { ID=1, Name="Scott", HireDate=new DateTime(2002, 3, 5)},
+                new Employee { ID=2, Name="Poonam", HireDate=new DateTime(2002, 10, 15)},
+                new Employee { ID=3, Name="Paul", HireDate=new DateTime(2007, 10, 11)}
+
+            };
+
+            IEnumerable<Employee> query =
+                from e in employees
+                where e.HireDate.Year < 2005
+                orderby e.Name
+                select e;
+
+            foreach (Employee e in query)
+            {
+                Console.WriteLine(e.Name);
+            }
+        }
+
+        class Employee
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public DateTime HireDate { get; set; }
+        }
+    }
+}
---
+++ src/Recipes.Common.DotNetFramework/Regex.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Recipes.Common.DotNetFramework
+{
+    class Regex
+    {
+        static void Tests()
+        {
+            List<string> patterns = new List<string>()
+            {
+                "[a-g]",
+                "[a-g]{3}",
+                "^[a-g]{3}$",
+                "^[a-g]{1,3}$",
+                "^[0-9]{8}$",
+                "^[a-z]{3}[0-9]{5}$",
+                "^[a-zA-Z]{3}[0-9]{5}$",
+            };
+
+            List<string> texts = new List<string>()
+            {
+                "aaaaa",
+                "1",
+                "aaa",
+                "a",
+                "agc",
+                "00000000",
+                "aaa00000",
+                "AAA00000"
+            };
+
+            ShowResult(patterns, texts);
+
+            Console.Read();
+        }
+
+        static void ShowResult(List<string> patterns, List<string> texts)
+        {
+            Regex obj;
+
+            foreach (string pattern in patterns)
+            {
+                obj = new Regex(pattern);
+                foreach (string text in texts)
+                {
+                    Console.WriteLine("Patter: {0}  str: {1} : {2}",
+                        pattern, text.PadRight(10, ' '), obj.IsMatch(text));
+                }
+                Console.WriteLine();
+            }
+        }
+
+        /* 
+        
+        []             - what characters we are going to use
+        {}             - how many characters we are going to use
+        ^              - start
+        $              - end
+        [a-g]{3}       - characters from a to g But aaaa - also have 'aaa' so that the expresion is valid
+        ^[a-g]{1,3}$   - min lenght 1 max 3
+        ^[0-9]{8}$     - number with 8 digits
+        ^[a-z]{3}[0-9]{5}$
+
+         */
+
+    }
+}
+}
